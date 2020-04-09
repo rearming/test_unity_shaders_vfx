e53912a175fb9ca0b5c5be99b873d626771ac156
sleonard <sleonard@student.21-school.ru>
2020-04-09 12:20:25 +0300

    added auto save, joystick and rpg monster pack, fixed camera

--- Assets/Scripts/CameraMoving.cs
+++ Assets/Scripts/CameraMoving.cs
@@ -13,12 +13,10 @@ public class CameraMoving : MonoBehaviour
     [SerializeField] private float maxVertAngle = 60.0f;
 
     private Transform _transform;
-    private Transform _cameraTransform;
 
     void Start()
     {
         _transform = transform;
-        _cameraTransform = GetComponentInChildren<Camera>().GetComponent<Transform>();
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
@@ -26,32 +24,21 @@ public class CameraMoving : MonoBehaviour
     void Update()
     {
         _transform.position += GetMovementVector();
-        _transform.localEulerAngles = GetRotationY();
-        _cameraTransform.localEulerAngles = GetRotationX();
+        _transform.localEulerAngles = GetRotation();
     }
 
-    private Vector3 GetRotationX()
+    private Vector3 GetRotation()
     {
-        var cameraRotation = _cameraTransform.localEulerAngles;
+        var cameraRotation = _transform.localEulerAngles;
         var rotation = cameraRotation;
 
-        rotation.x -= Input.GetAxis("Mouse Y"); // vertical
-        rotation.x = Mathf.Lerp(cameraRotation.x, rotation.x, Time.deltaTime * rotateSensVert);
+        rotation.x -= Input.GetAxis("Mouse Y") * rotateSensVert; // vertical
         rotation.x = rotation.x > 180 ? rotation.x - 360 : rotation.x;
         rotation.x = Mathf.Clamp(rotation.x, -maxVertAngle, maxVertAngle);
+        rotation.y += Input.GetAxis("Mouse X") * rotateSensHoriz; // horizontal
         return rotation;
     }
 
-    private Vector3 GetRotationY()
-    {
-        var transformRotation = _transform.localEulerAngles;
-        var rotation = transformRotation;
-        
-        rotation.y += Input.GetAxis("Mouse X"); // horizontal
-        rotation.y = Mathf.Lerp(transformRotation.y, rotation.y, Time.deltaTime * rotateSensHoriz);
-        return rotation;
-    }
-    
     private Vector3 GetMovementVector()
     {
         var moveForward = _transform.forward;
